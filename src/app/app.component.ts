e17abaea61599b0612f0ed1a14bbd266db9a3dd0
Vikram Dharajiya <vikram.dharajiya@tatvasoft.com>
2025-09-25 09:50:26 +0530

    NG19L-001 Create Profile Custome Component
    NG19L-001 Create Profile Custome Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, SignupComponent],
+  imports: [RouterOutlet, LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app-profile",
+    template:`<h1>App Profile</h1>`,
+    styles:`
+        h1{
+            color:red;
+            background-color:lightyellow;
+        }
+    `
+})
+
+export class ProfileComponent{
+
+}
