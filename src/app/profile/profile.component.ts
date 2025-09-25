0e98a0a10f8f63c6d9800015166b249b1b1f27c2
Vikram Dharajiya <vikram.dharajiya@tatvasoft.com>
2025-09-25 09:56:31 +0530

    NG19L-001 add html template adn css file instead of  inline
    NG19L-001 add html template adn css file instead of  inline

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,13 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-profile",
-    template:`<h1>App Profile</h1>`,
-    styles:`
-        h1{
-            color:red;
-            background-color:lightyellow;
-        }
-    `
+    templateUrl:"./profile.component.html",
+    styleUrl:"./profile.component.css"
 })
 
 export class ProfileComponent{
