593dd98ab7bc9e846a8fc50e8d928d4b2bd2c84f
Vikram Dharajiya <vikram.dharajiya@tatvasoft.com>
2025-09-25 09:36:47 +0530

    NG19L-001 Generate Sign Up Component and used it in app component
    NG19L-001 Generate Sign Up Component and used it in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
